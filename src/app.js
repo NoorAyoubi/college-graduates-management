ea191d9223fa7c5e60842ecae4f4d371445da32f
NoorAyoubi <lightsun16@yahoo.com>
2026-01-04 21:07:07 +0200

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCreIVkdI5OSJoMpdWN1xqYNBYuOOkieT8",
+  authDomain: "college-website-39f21.firebaseapp.com",
+  projectId: "college-website-39f21",
+  storageBucket: "college-website-39f21.firebasestorage.app",
+  messagingSenderId: "338105646562",
+  appId: "1:338105646562:web:96aab763dcbfa38c3bcdbb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
